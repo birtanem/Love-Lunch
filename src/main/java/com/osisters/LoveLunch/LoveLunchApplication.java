7c6ddc55f648bde8d8f5b018f8f2eb82e852fe50
geunii <geunii@mz.co.kr>
2024-02-01 13:31:10 +0900

    Init Project

---
+++ src/main/java/com/osisters/LoveLunch/LoveLunchApplication.java
@@ -0,0 +1,13 @@
+package com.osisters.LoveLunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveLunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveLunchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osisters/LoveLunch/LoveLunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osisters.LoveLunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveLunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
